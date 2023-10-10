9d61e7381b3308d218cc360a36a140f7c55a4c91
Şamil Nugay <samil_nugay@hotmail.com>
2023-10-10 23:54:35 +0300

    first commit

---
+++ lib/controller/scan_controller.dart
@@ -0,0 +1,80 @@
+import 'package:camera/camera.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ScanController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    initCamera();
+    initTFLite();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cameraController.dispose();
+  }
+
+  late CameraController cameraController;
+  late List<CameraDescription> cameras;
+
+  var isCameraInitialized = false.obs;
+  var cameraCount = 0;
+
+  initCamera() async {
+    if (await Permission.camera.request().isGranted) {
+      cameras = await availableCameras();
+
+      cameraController = CameraController(cameras[0], ResolutionPreset.max);
+      await cameraController.initialize().then((value) {
+        cameraCount++;
+        if (cameraCount % 10 == 0) {
+          cameraCount = 0;
+          cameraController.startImageStream((image) {
+            cameraCount++;
+            if (cameraCount % 10 == 0) {
+              cameraCount = 0;
+              objectDetector(image);
+            }
+            update();
+          });
+          update();
+        }
+      });
+      isCameraInitialized(true);
+      update();
+    } else {
+      print('Permission Denied');
+    }
+  }
+
+  initTFLite() async {
+    await Tflite.loadModel(
+        model: "assets/model_unquant.tflite",
+        labels: "assets/labels.txt",
+        isAsset: true,
+        numThreads: 1,
+        useGpuDelegate: false);
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+      bytesList: image.planes.map((e) {
+        return e.bytes;
+      }).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+    if (detector != null) {
+      print('Result is $detector');
+    }
+  }
+}
